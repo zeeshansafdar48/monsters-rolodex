72640472e9c936108ed2673d31d814b8abe090b8
Zeeshan Safdar <zeeshan7826@gmail.com>
2021-04-06 10:19:54 +0500

    json placeholder api call and render names of the users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [monsters, setMonsters] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {monsters?.map((monster, index) => {
+        return (
+          <div key={index}>
+            <h2>{monster?.name}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
